5c32ddbf761418c56db690c43df23d3072fd67ba
Enrique Corpus <enriquecorpus@skydev.solutions>
2019-07-10 11:23:46 +0800

    add cefsharp chromium browser

--- ComplianceLogger/ComplianceLogger/Form1.cs
+++ ComplianceLogger/ComplianceLogger/Form1.cs
@@ -7,9 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
 
 namespace ComplianceLogger
 {
+    public ChromiumWebBrowser chromeBrowser;
+
+    public void InitializeChromium()
+    {
+        CefSettings settings = new CefSettings();
+        // Initialize cef with the provided settings
+        Cef.Initialize(settings);
+        // Create a browser component
+        chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
+        // Add it to the form and fill it to the form window.
+        this.Controls.Add(chromeBrowser);
+        chromeBrowser.Dock = DockStyle.Fill;
+    }
+
     public partial class Form1 : Form
     {
         public Form1()

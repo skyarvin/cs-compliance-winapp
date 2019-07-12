5bd116ddd5166796bb47164f2e627bb074b28df5
Enrique Corpus <enriquecorpus@skydev.solutions>
2019-07-12 17:05:29 +0800

    Fix, initial test success

--- CefSharpTestBrowser/WindowsFormsApp1/Program.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Models;
+using WindowsFormsApp1.Services;
 
 namespace WindowsFormsApp1
 {
@@ -16,7 +19,39 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException +=  new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            var Login = new frmLogin();
+            Login.Show();
+            Application.Run();
+        }
+
+        static void Application_ThreadException
+        (object sender, System.Threading.ThreadExceptionEventArgs e)
+        {// All exceptions thrown by the main thread are handled over this method
+
+            ShowExceptionDetails(e.Exception);
+        }
+
+        [Obsolete]
+        static void CurrentDomain_UnhandledException
+            (object sender, UnhandledExceptionEventArgs e)
+        {// All exceptions thrown by additional threads are handled in this method
+
+            ShowExceptionDetails(e.ExceptionObject as Exception);
+
+            // Suspend the current thread for now to stop the exception from throwing.
+            Thread.CurrentThread.Suspend();
+        }
+
+        static void ShowExceptionDetails(Exception Ex)
+        {
+            // Do logging of exception details
+            MessageBox.Show(Ex.Message, Ex.TargetSite.ToString(),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            LoggerServices.SaveToLogFile(string.Concat(Ex.Message, Ex.TargetSite.ToString()), true);
         }
     }
 }

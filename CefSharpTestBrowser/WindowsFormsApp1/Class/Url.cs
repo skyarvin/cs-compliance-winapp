5f3f8ef4a58d57edd20d81ef100f074c3ba8fe93
Mark Borres <markborres@skydev.solutions>
2019-08-01 09:41:10 +0800

    Refactor URLs

---
+++ CefSharpTestBrowser/WindowsFormsApp1/Class/Url.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkydevCSTool.Class
+{
+    public static class Url
+    {
+        public static string CB_COMPLIANCE_URL = "https://chaturbate.com/compliance";
+        public static string CB_COMPLIANCE_SET_ID_EXP_URL = "https://chaturbate.com/compliance/update_expiration_date_form";
+        public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
+    }
+}

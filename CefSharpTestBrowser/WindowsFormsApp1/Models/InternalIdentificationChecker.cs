e69e415034d8ff07f1d4789c30d693b84447facc
skyPheeJae <87847797+skyPheeJae@users.noreply.github.com>
2023-09-18 10:29:29 +0800

    :sparkles: add iidc cs file

---
+++ CefSharpTestBrowser/WindowsFormsApp1/Models/InternalIdentificationChecker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using CSTool.Class;
+using System.Net.Http;
+using WindowsFormsApp1;
+
+namespace CSTool.Models
+{
+    public class InternalIdentificationChecker
+    {
+        public int id { get; set; }
+        public string url { get; set; }
+        public int agent_id { get; set; }
+        public string agent_notes { get; set; }
+        public string agent_uploaded_photo { get; set; }
+        public string agent_uploaded_photo_base64 { get; set; }
+        public string status { get; set; }
+        public string reviewer_notes { get; set; }
+        public string reviewer_uploaded_photo { get; set; }
+        public string updated_at { get; set; }
+        public int duration { get; set; }
+
+        public InternalIdentificationChecker Save()
+        {
+            Globals.SaveToLogFile(string.Concat("Save IIDC: ", JsonConvert.SerializeObject(this)), (int)LogType.Action);
+            using (var client = new HttpClient())
+            {
+                var uri = string.Concat(Url.API_URL, "/iidc/");
+                client.DefaultRequestHeaders.Add("Authorization", Globals.apiKey);
+                client.Timeout = TimeSpan.FromSeconds(5);
+                var content = new StringContent(JsonConvert.SerializeObject(this), Encoding.UTF8, "application/json");
+                var response = client.PostAsync(uri, content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    using (HttpContent data = response.Content)
+                    {
+                        var jsonString = data.ReadAsStringAsync();
+                        jsonString.Wait();
+                        return JsonConvert.DeserializeObject<InternalIdentificationChecker>(jsonString.Result);
+                    }
+                }
+                else
+                {
+                    Globals.SaveToLogFile(JsonConvert.SerializeObject(this), (int)LogType.Error);
+                    throw new Exception("Api save iidc request error, Please contact dev team");
+                }
+            }
+        }
+
+        public static InternalIdentificationChecker Get(int iidc_id, int agent_id)
+        {
+
+            using (var client = new HttpClient())
+            {
+                var uri = string.Concat(Url.API_URL, "/iidc/", iidc_id, "/", agent_id, "/");
+                client.DefaultRequestHeaders.Add("Authorization", Globals.apiKey);
+                using (HttpResponseMessage response = client.SendAsync(new HttpRequestMessage(HttpMethod.Get, uri)).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            var jsonString = content.ReadAsStringAsync();
+                            jsonString.Wait();
+                            return JsonConvert.DeserializeObject<InternalIdentificationChecker>(jsonString.Result);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static InternalIdentificationChecker GetAgentIIDC(int agent_id)
+        {
+            using (var client = new HttpClient())
+            {
+                var uri = string.Concat(Url.API_URL, "/iidc/agent/", agent_id, "/");
+                client.DefaultRequestHeaders.Add("Authorization", Globals.apiKey);
+                using (HttpResponseMessage response = client.SendAsync(new HttpRequestMessage(HttpMethod.Get, uri)).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            var jsonString = content.ReadAsStringAsync();
+                            jsonString.Wait();
+                            return JsonConvert.DeserializeObject<InternalIdentificationChecker>(jsonString.Result);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}

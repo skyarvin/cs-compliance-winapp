79326473325044c3d9aebac28ddbac998ad87ca6
skyPheeJae <87847797+skyPheeJae@users.noreply.github.com>
2023-09-25 10:48:36 +0800

    Staffme: [Tool] Enable Tool CAM (#58)
    * :sparkles: implement camera capture
    
    * :art: add condition to trigger camera capture
    
    * :pencil2: fix typos and remove excess spaces and comments
    
    * :art: refactor nested if statement
    
    * :pencil2: remove excess spaces
    
    * :art: remove unused try catch
    
    * :art: add this to all instance variables
    
    ---------
    
    Co-authored-by: skyberwyn <berwynfelismenia@skydev.solutions>

---
+++ CefSharpTestBrowser/WindowsFormsApp1/Class/StaffCam.cs
@@ -0,0 +1,103 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+
+namespace CSTool.Class
+{
+    public class StaffCam
+    {
+        FilterInfoCollection videoDevices = null;
+        VideoCaptureDevice videoSource = null;
+        System.Windows.Forms.PictureBox imgVideo = null;
+        Bitmap bmpVideo = null;
+
+        public StaffCam()
+        {
+            loadCameraSource();
+        }
+
+        public void SetVideoPlaceHolder(System.Windows.Forms.PictureBox videoPlaceHolder)
+        {
+            this.imgVideo = videoPlaceHolder;
+        }
+
+        private void loadCameraSource()
+        {
+            try
+            {
+                this.videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            }
+            catch (ApplicationException)
+            {
+                this.videoDevices = null;
+            }
+        }
+
+        public FilterInfoCollection getVideoDevices()
+        {
+            return this.videoDevices;
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+            if (this.imgVideo != null)
+            {
+                this.imgVideo.Image = img;
+            }
+            this.bmpVideo = img;
+        }
+
+        public void stopCamera()
+        {
+            if (this.videoSource != null && this.videoSource.IsRunning)
+            {
+                this.videoSource.SignalToStop();
+                this.videoSource = null;
+            }
+        }
+
+        public void startCamera(string source)
+        {
+            try
+            {
+                this.videoSource = new VideoCaptureDevice(source);
+                if (this.videoSource != null)
+                {
+                    this.videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                    this.videoSource.Start();
+                }
+            }
+            catch
+            {
+                stopCamera();
+            }
+        }
+
+        public bool isRunning()
+        {
+            if (this.videoSource != null && this.videoSource.IsRunning)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void captureImage(string filename)
+        {
+            if (this.bmpVideo != null)
+            {
+                try
+                {
+                    Bitmap resized = new Bitmap(this.bmpVideo, new Size(this.bmpVideo.Width / 4, this.bmpVideo.Height / 4));
+                    resized.Save(filename);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}

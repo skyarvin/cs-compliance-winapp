181ca2082c31d81f9e364df008557ba26dd5fcd1
skyberwyn <berwynfelismenia@skydev.solutions>
2023-10-10 09:51:17 +0800

    :art: rafactor 2fa

--- CefSharpTestBrowser/WindowsFormsApp1/frmMfa.Designer.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/frmMfa.Designer.cs
@@ -74,7 +74,7 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(23, 227);
+            this.button1.Location = new System.Drawing.Point(23, 226);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(331, 36);
             this.button1.TabIndex = 4;

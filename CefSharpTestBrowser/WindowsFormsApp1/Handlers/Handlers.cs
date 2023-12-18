bdf7f863b7d4eea3fb35fbdc18bf4666e4157649
skyRoi <roygopez@skydev.solutions>
2023-12-18 15:34:04 +0800

    :adhesive_bandage: fix breaking changes

--- CefSharpTestBrowser/WindowsFormsApp1/Handlers/Handlers.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/Handlers/Handlers.cs
@@ -153,5 +153,20 @@ namespace CSTool.Handlers
         {
             throw new NotImplementedException();
         }
+
+        public void OnDocumentAvailableInMainFrame(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+
+        }
+
+        public IResourceRequestHandler GetResourceRequestHandler(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, bool isNavigation, bool isDownload, string requestInitiator, ref bool disableDefaultHandling)
+        {
+            return null;
+        }
+
+        public bool GetAuthCredentials(IWebBrowser chromiumWebBrowser, IBrowser browser, string originUrl, bool isProxy, string host, int port, string realm, string scheme, IAuthCallback callback)
+        {
+            return true;
+        }
     }
 }

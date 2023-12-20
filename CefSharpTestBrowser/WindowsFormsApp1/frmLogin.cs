2c743371c265e72492855407c9ce73edf7287600
skyRoi <roygopez@skydev.solutions>
2023-12-20 10:21:32 +0800

    :art: add comment in frmLogin clear cache

--- CefSharpTestBrowser/WindowsFormsApp1/frmLogin.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/frmLogin.cs
@@ -142,6 +142,8 @@ namespace WindowsFormsApp1
             string cache_path = string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "/cache/cache/");
             if (Directory.Exists(cache_path))
             {
+                //delete all except LocalPrefs.json
+                //contains encryption key required for Cookie persistence
                 DirectoryInfo cache_path_di = new DirectoryInfo(cache_path);
                 foreach (DirectoryInfo cache_directories in cache_path_di.GetDirectories().Where(directory => directory.Name != "Network"))
                     cache_directories.Delete(true);

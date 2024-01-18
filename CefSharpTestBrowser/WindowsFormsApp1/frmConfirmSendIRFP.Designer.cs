481482b53a6cb5a4509a512714c5a4f957bcd397
skyRoi <roygopez@skydev.solutions>
2024-01-18 10:44:50 +0800

    :lipstick: set AutoScaleMode of frmConfirmSendIRFP to DPI

--- CefSharpTestBrowser/WindowsFormsApp1/frmConfirmSendIRFP.Designer.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/frmConfirmSendIRFP.Designer.cs
@@ -90,8 +90,8 @@
             // 
             // frmConfirmSendIRFP
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(95)))), ((int)(((byte)(167)))));
             this.ClientSize = new System.Drawing.Size(592, 159);
             this.Controls.Add(this.BtnNo);

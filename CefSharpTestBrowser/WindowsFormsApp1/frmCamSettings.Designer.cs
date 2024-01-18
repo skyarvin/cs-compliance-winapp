0047b8f9e70c42ce4abcc71038413a15e9addd03
skyRoi <roygopez@skydev.solutions>
2024-01-18 10:49:10 +0800

    :lipstick: set AutoScaleMode of frmCamSettings to DPI

--- CefSharpTestBrowser/WindowsFormsApp1/frmCamSettings.Designer.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/frmCamSettings.Designer.cs
@@ -105,8 +105,8 @@
             // 
             // frmCamSettings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(473, 305);
             this.Controls.Add(this.btnRefresh);

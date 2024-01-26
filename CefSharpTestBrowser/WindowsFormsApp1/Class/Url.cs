9cbda3fc092254b32c54706fd1eb041cfd87fcf8
skyJezreel <jezreelmartin@skydev.solutions>
2024-01-26 11:17:48 +0800

    :rewind:

--- CefSharpTestBrowser/WindowsFormsApp1/Class/Url.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/Class/Url.cs
@@ -14,14 +14,14 @@ namespace CSTool.Class
         //####################################################################################//
 
         // ## Production CSCB-Staff Me
-        //public static string CB_HOME = "https://chaturbate.com/";
-        //public static string CB_COMPLIANCE_URL = "https://chaturbate.com/compliance";
-        //public static string CB_COMPLIANCE_SET_ID_EXP_URL = "https://chaturbate.com/compliance/update_expiration_date_form";
-        //public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
-        //public static string BASE_URL = "https://cscb.staffme.online";
-        //public static string API_URL = BASE_URL + "/api";
-        //public static string AUTH_URL = BASE_URL + "/security";
-        //public static string DOMAIN = "chaturbate.com";
+        public static string CB_HOME = "https://chaturbate.com/";
+        public static string CB_COMPLIANCE_URL = "https://chaturbate.com/compliance";
+        public static string CB_COMPLIANCE_SET_ID_EXP_URL = "https://chaturbate.com/compliance/update_expiration_date_form";
+        public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
+        public static string BASE_URL = "https://cscb.staffme.online";
+        public static string API_URL = BASE_URL + "/api";
+        public static string AUTH_URL = BASE_URL + "/security";
+        public static string DOMAIN = "chaturbate.com";
 
         // ## Production Volume
         //public static string CB_HOME = "https://volume.com/";
@@ -39,13 +39,13 @@ namespace CSTool.Class
         //public static string API_URL = "https://cscb.staffme.online/api";
 
         // ## Pure Local Testing with MockSite
-        public static string CB_HOME = "http://127.0.0.1:8000/compliance/4/show/razer";
-        public static string DOMAIN = "http://127.0.0.1:8000/compliance";
-        public static string CB_COMPLIANCE_URL = "http://127.0.0.1:8000/compliance";
-        public static string CB_COMPLIANCE_SET_ID_EXP_URL = "http://127.0.0.1:8000/compliance/update_expiration_date_form";
-        public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
-        public static string BASE_URL = "http://127.0.0.1:9000";
-        public static string API_URL = BASE_URL + "/api";
-        public static string AUTH_URL = BASE_URL + "/security";
+        //public static string CB_HOME = "http://127.0.0.1:8000/compliance/4/show/razer";
+        //public static string DOMAIN = "http://127.0.0.1:8000/compliance";
+        //public static string CB_COMPLIANCE_URL = "http://127.0.0.1:8000/compliance";
+        //public static string CB_COMPLIANCE_SET_ID_EXP_URL = "http://127.0.0.1:8000/compliance/update_expiration_date_form";
+        //public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
+        //public static string BASE_URL = "http://127.0.0.1:9000";
+        //public static string API_URL = BASE_URL + "/api";
+        //public static string AUTH_URL = BASE_URL + "/security";
     }
 }

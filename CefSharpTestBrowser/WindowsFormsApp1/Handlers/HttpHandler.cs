fcea064505215a8bdfea634445d957cd9e90ca76
skyMarvs <marvingregorio@skydev.solutions>
2024-02-07 15:35:40 +0800

    :art: add os to the headers

--- CefSharpTestBrowser/WindowsFormsApp1/Handlers/HttpHandler.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/Handlers/HttpHandler.cs
@@ -101,15 +101,15 @@ namespace CSTool.Handlers
             DefaultRequestHeaders.Add("Staffme-Authorization", Globals.apiKey);
             if (!public_routes.Contains(new Uri(requestUri).AbsolutePath))
             {
-                Console.WriteLine("testtest : " + requestUri);
                 DefaultRequestHeaders.Add("Authorization", Globals.UserToken.access_token);
             }
 
             RegistryKey localMachine = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
             RegistryKey windowsNTKey = localMachine.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion");
             var productID = windowsNTKey.GetValue("ProductId");
+            var operatingSystem = windowsNTKey.GetValue("ProductName");
             DefaultRequestHeaders.Add("Device-Id", $"{productID}");
-            DefaultRequestHeaders.Add("Ip-Address", $"{productID}");
+            DefaultRequestHeaders.Add("OS", $"{operatingSystem}");
         }
     }
 }

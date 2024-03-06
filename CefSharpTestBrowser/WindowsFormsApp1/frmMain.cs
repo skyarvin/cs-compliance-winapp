0cc9126130990b0b35a31c34b50916174ef80352
skyMarvs <marvingregorio@skydev.solutions>
2024-03-06 13:37:53 +0800

    :recycle: reduce allowable wasted time to 5 seconds

--- CefSharpTestBrowser/WindowsFormsApp1/frmMain.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/frmMain.cs
@@ -706,7 +706,7 @@ namespace WindowsFormsApp1
                 }
 
                 var actual_start_time = last_action;
-                var allowable_wasted_time = 30;
+                var allowable_wasted_time = 5;
                 if((start_time - last_action).TotalSeconds  >= allowable_wasted_time)
                 {
                     actual_start_time = start_time;

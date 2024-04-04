fca1d394235affc1f43a2617e21b314749d62554
skypheejae <psalas@skydev.solutions>
2024-04-04 14:18:36 +0800

    :art: refactor url setup

--- CefSharpTestBrowser/WindowsFormsApp1/Class/Url.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/Class/Url.cs
@@ -10,13 +10,15 @@ namespace CSTool.Class
     {
         //##################SET TO FALSE WHEN DEPLOYING ON PRODUCTION #########################//
         public static bool DEBUG = false;
+        public static bool DEV_API = false;
         public static string KNOWLEDGE_BASE_URL = "https://staffme.online/knowledge-base/";
         //####################################################################################//
+        
 
         // ## Production CSCB-Staff Me
         public static string CB_HOME = "https://chaturbate.com/";
-        public static string CB_COMPLIANCE_URL = "https://chaturbate.com/compliance";
-        public static string CB_COMPLIANCE_SET_ID_EXP_URL = "https://chaturbate.com/compliance/update_expiration_date_form";
+        public static string CB_COMPLIANCE_URL = CB_HOME + "compliance";
+        public static string CB_COMPLIANCE_SET_ID_EXP_URL = CB_HOME + "compliance/update_expiration_date_form";
         public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
         public static string BASE_URL = "https://cscb.staffme.online";
         public static string API_URL = BASE_URL + "/api";
@@ -25,27 +27,38 @@ namespace CSTool.Class
 
         // ## Production Volume
         //public static string CB_HOME = "https://volume.com/";
-        //public static string CB_COMPLIANCE_URL = "https://volume.com/compliance/";
-        //public static string CB_COMPLIANCE_SET_ID_EXP_URL = "https://volume.com/compliance/update_expiration_date_form";
+        //public static string CB_COMPLIANCE_URL = CB_HOME + "compliance";
+        //public static string CB_COMPLIANCE_SET_ID_EXP_URL = CB_HOME + "compliance/update_expiration_date_form";
         //public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
         //public static string API_URL = "https://csvolume.skydev.solutions/api";
         //public static string DOMAIN = "volume.com";
 
-        // ## Local Testing with Live Rooms
-        //public static string CB_HOME = "https://chaturbate.com/";
-        //public static string CB_COMPLIANCE_URL = "https://chaturbate.com/compliance";
-        //public static string CB_COMPLIANCE_SET_ID_EXP_URL = "https://chaturbate.com/compliance/update_expiration_date_form";
-        //public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
-        //public static string API_URL = "https://cscb.staffme.online/api";
+        static Url()
+        {
+            if (DEBUG && !DEV_API)
+            {
+                // ## Pure Local Testing with MockSite
+                CB_HOME = "http://127.0.0.1:8000/";
+                BASE_URL = "http://127.0.0.1:9000";
+                DOMAIN = "http://127.0.0.1:8000/compliance";
+            }
+            else if (DEBUG && DEV_API)
+            {
+                // ## Dev Testing with MockSite
+                CB_HOME = "http://127.0.0.1:8000/";
+                BASE_URL = "https://cscb-dev1.staffme.online";
+                DOMAIN = "http://127.0.0.1:8000/compliance";
+            }
+            else if (!DEBUG && DEV_API)
+            {
+                // ## Dev Testing with Live Rooms
+                BASE_URL = "https://cscb-dev1.staffme.online";
+            }
 
-        // ## Pure Local Testing with MockSite
-        //public static string CB_HOME = "http://127.0.0.1:8000/compliance/show/razer";
-        //public static string DOMAIN = "http://127.0.0.1:8000/compliance";
-        //public static string CB_COMPLIANCE_URL = "http://127.0.0.1:8000/compliance";
-        //public static string CB_COMPLIANCE_SET_ID_EXP_URL = "http://127.0.0.1:8000/compliance/update_expiration_date_form";
-        //public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
-        //public static string BASE_URL = "http://127.0.0.1:9000";
-        //public static string API_URL = BASE_URL + "/api";
-        //public static string AUTH_URL = BASE_URL + "/security";
+            CB_COMPLIANCE_URL = CB_HOME + "compliance";
+            CB_COMPLIANCE_SET_ID_EXP_URL = CB_HOME + "compliance/update_expiration_date_form";
+            API_URL = BASE_URL + "/api";
+            AUTH_URL = BASE_URL + "/security";
+        }
     }
 }

d6cf50a69692cc72ed5ec1d7de7b096dfef332b8
skypheejae <psalas@skydev.solutions>
2024-04-08 15:01:43 +0800

    :art: refactor Url.cs

--- CefSharpTestBrowser/WindowsFormsApp1/Class/Url.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/Class/Url.cs
@@ -13,16 +13,16 @@ namespace CSTool.Class
         public static bool DEV_API = false;
         public static string KNOWLEDGE_BASE_URL = "https://staffme.online/knowledge-base/";
         //####################################################################################//
-        
+
 
         // ## Production CSCB-Staff Me
         public static string CB_HOME = "https://chaturbate.com/";
-        public static string CB_COMPLIANCE_URL = CB_HOME + "compliance";
-        public static string CB_COMPLIANCE_SET_ID_EXP_URL = CB_COMPLIANCE_URL + "/update_expiration_date_form";
+        public static string CB_COMPLIANCE_URL;
+        public static string CB_COMPLIANCE_SET_ID_EXP_URL;
         public static string GOOGLE_TRANSLATE_URL = "https://translate.google.com?text=";
         public static string BASE_URL = "https://cscb.staffme.online";
-        public static string API_URL = BASE_URL + "/api";
-        public static string AUTH_URL = BASE_URL + "/security";
+        public static string API_URL;
+        public static string AUTH_URL;
         public static string DOMAIN = "chaturbate.com";
 
         // ## Production Volume
@@ -38,8 +38,6 @@ namespace CSTool.Class
             if (DEBUG)
             {
                 CB_HOME = "http://127.0.0.1:8000/";
-                CB_COMPLIANCE_URL = CB_HOME + "compliance";
-                CB_COMPLIANCE_SET_ID_EXP_URL = CB_COMPLIANCE_URL + "/update_expiration_date_form";
                 BASE_URL = "http://127.0.0.1:9000";
                 DOMAIN = "http://127.0.0.1:8000/compliance";
             }
@@ -48,7 +46,9 @@ namespace CSTool.Class
             {
                 BASE_URL = "https://cscb-dev1.staffme.online";
             }
-            
+
+            CB_COMPLIANCE_URL = CB_HOME + "compliance";
+            CB_COMPLIANCE_SET_ID_EXP_URL = CB_COMPLIANCE_URL + "/update_expiration_date_form";
             API_URL = BASE_URL + "/api";
             AUTH_URL = BASE_URL + "/security";
         }

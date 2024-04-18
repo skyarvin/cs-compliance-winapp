ad3c86de17f6dd0895f88d52fee568fe50f36068
skypheejae <psalas@skydev.solutions>
2024-04-18 15:59:31 +0800

    :art: refactor code

--- CefSharpTestBrowser/WindowsFormsApp1/Bound.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/Bound.cs
@@ -62,18 +62,6 @@ namespace CSTool
                    }
 
                    window.onclick = function(e) {
-                        var free_photos_element_values = {
-                            'Approve': ['approve_button'], 
-                            'Reject': ['reject'], 
-                            'Submit': ['request-review-submit']
-                        }
-
-                        if (free_photos_element_values.hasOwnProperty(e.target.value)) {
-                            var notes = document.getElementById('review_reason').value;
-                            bound.onClicked(free_photos_element_values[e.target.value][0], notes, violation);
-                            return;
-                        }
-
                         if (e.target.id != null || e.target.id.length > 0 || e.target.name || e.target.value) { 
                             bound.windowOnClicked(e.target.id + '::[name]='+e.target.name+'::[value]='+e.target.value+'::[url]='+ window.location.href ); 
                         }
@@ -82,6 +70,7 @@ namespace CSTool
                             var element_ids = ['approve_button','violation-submit','spammer-submit','request-review-submit','agree_button','disagree_button','reply_button','request_photo_button'];
                             var violation = $('#id_violation option:selected').text();
                             var notes = $('#id_description').val();
+
                             if (element_ids.includes(e.target.id)) {
                                 console.log(e.target.id);
                                 bound.onClicked(e.target.id, notes, violation);
@@ -89,13 +78,18 @@ namespace CSTool
                             }
 
                             var element_values = {
-                                  'Update Expiration Date': ['set_expr'],
-                                  'Report Identification Missing Problem': ['id-missing'],
-                                  'Change Gender': ['change_gender']
+                                'Update Expiration Date': 'set_expr',
+                                'Report Identification Missing Problem': 'id-missing',
+                                'Change Gender': 'change_gender',
+                                'Approve': 'approve_button', 
+                                'Reject': 'reject', 
+                                'Submit': 'request-review-submit'
                             }
 
                             if (element_values.hasOwnProperty(e.target.value)) {
-                                    bound.onClicked(element_values[e.target.value][0], notes, violation);
+                                if (e.target.value === 'Submit')
+                                    notes = $('#review_reason').val();
+                                bound.onClicked(element_values[e.target.value], notes, violation);
                             }
                         }
                     }

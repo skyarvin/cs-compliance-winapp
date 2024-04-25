db4fd2e5724bcd8f6c8c96517e2e34b55e255976
skyMarvs <marvingregorio@skydev.solutions>
2024-04-25 11:10:25 +0800

    :art: update tool version to 6.8.0

--- CefSharpTestBrowser/WindowsFormsApp1/Properties/AssemblyInfo.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.7.0")]
-[assembly: AssemblyFileVersion("6.7.0")]
+[assembly: AssemblyVersion("6.8.0")]
+[assembly: AssemblyFileVersion("6.8.0")]
 
 //History Publish Version
 //1.0.0.0   Initial working installer with auto update

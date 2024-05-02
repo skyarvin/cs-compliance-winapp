c303ed4aa16af2e86fab8a19c5c9cf9b8c31dc22
skyJezreel <jezreelmartin@skydev.solutions>
2024-05-02 11:41:45 +0800

    add support for chat room

--- CefSharpTestBrowser/WindowsFormsApp1/frmMain.cs
+++ CefSharpTestBrowser/WindowsFormsApp1/frmMain.cs
@@ -56,7 +56,9 @@ namespace WindowsFormsApp1
             {Action.ChatReply.Value, "AP" },
             {Action.RequestFacePhoto.Value, "RFP" },
             {Action.Reject.Value, "RJ"},
-            {Action.RejectHasNudity.Value, "RN"},
+            {Action.RejectHasNudity.Value, "RHN"},
+            {Action.MarkAsHacked.Value, "MH" },
+            {Action.GhostSpammer.Value, "GS" }
         };
 
         private List<string> Violations = new List<string>
@@ -65,7 +67,8 @@ namespace WindowsFormsApp1
            Action.IdMissing.Value,
            Action.SpammerSubmit.Value,
            Action.RequestReview.Value,
-           Action.RequestFacePhoto.Value
+           Action.RequestFacePhoto.Value,
+           Action.MarkAsHacked.Value,
         };
 
         #region Init
@@ -943,6 +946,8 @@ namespace WindowsFormsApp1
             public static Action RequestFacePhoto { get { return new Action("request_photo_button"); } }
             public static Action Reject { get { return new Action("reject"); } }
             public static Action RejectHasNudity { get { return new Action("nudity_verifyer_reject"); } }
+            public static Action MarkAsHacked { get { return new Action("mark_hacked"); } }
+            public static Action GhostSpammer { get { return new Action("ghost_spammer"); } }
         }
 
         private void BgWorkResync_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
@@ -1067,7 +1072,7 @@ namespace WindowsFormsApp1
 
         public bool IsComplianceUrl(string url)
         {
-            if (url.Contains("compliance") && Regex.IsMatch(url, "show|photoset|chat_media|exhibitionist|notification_photoset")) 
+            if (url.Contains("compliance") && Regex.IsMatch(url, "show|photoset|chat_media|exhibitionist|notification_photoset|chat")) 
                 return true;
 
             return false;
@@ -1989,6 +1994,10 @@ namespace WindowsFormsApp1
             else if (Globals.ComplianceAgent.room_type == "NOTIFICATION_PHOTOSET")
             {
                 Globals.chromeBrowser.Load(string.Concat(Url.CB_COMPLIANCE_URL, "/notification_photoset/"));
+            }
+            else if (Globals.ComplianceAgent.room_type == "CHAT") 
+            {
+                Globals.chromeBrowser.Load(string.Concat(Url.CB_COMPLIANCE_URL, "/chat/"));
             }
         }
     }
